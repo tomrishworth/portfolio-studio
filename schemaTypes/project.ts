64cb09301d1f527f2d61bd34839cd4644d7f8056
Tom Rishworth <tomrishworth@gmail.com>
2025-04-28 21:49:47 +1200

    Init

---
+++ schemaTypes/project.ts
@@ -0,0 +1,28 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{type: 'image', options: {hotspot: true}}],
+      validation: (Rule) => Rule.required().min(1),
+    }),
+  ],
+})
